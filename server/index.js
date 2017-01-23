c5f922f245febaf04fd8f059a43ab5f15a855bdf
Rene Ramirez <rene.szabo@gmail.com>
2017-01-22 20:08:53 -0500

    Add error listener - redirect to error page

--- server/index.js
+++ server/index.js
@@ -35,6 +35,24 @@ app.get('/search', function (req, res) {
     res.render('search', { })
 })
 // --- ------------------------------------------------------------------------------------------------------------- ---
+// --- catch 404 and forward to error handler
+// --- ------------------------------------------------------------------------------------------------------------- ---
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+// --- ------------------------------------------------------------------------------------------------------------- ---
 // --- Socket IO Settings
 // --- ------------------------------------------------------------------------------------------------------------- ---
 io.on('connection', function (socket) {
